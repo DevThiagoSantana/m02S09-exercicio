19098f234cd02161429ee73cb35a5b2e009b882a
DevThiagoSantana <113400543+DevThiagoSantana@users.noreply.github.com>
2023-03-23 10:02:13 -0300

    Exercicio02

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
+import { GlobalStateProvider } from "./hooks/useGlobalState";
 import Navbar from './components/Navbar';
 import PageWrapper from './components/PageWrapper';
 import Footer from './components/Footer';
-
 import HomePage from './pages/HomePage';
+import { useState } from "react";
 
 function App() {
+  const [globalState,setGlobalState] =useState({
+    user:{
+      nome:"thiago",
+      isAdmin:true,
+    }
+  });
   return (
-    <>
+    <GlobalStateProvider value={[globalState,setGlobalState]}>
       <Navbar />
 
       <PageWrapper>
@@ -14,7 +21,7 @@ function App() {
       </PageWrapper>
 
       <Footer />
-    </>
+    </GlobalStateProvider>
   );
 }
 
