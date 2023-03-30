290084ad7b0931ee916099a5fd548325b90e372a
DevThiagoSantana <113400543+DevThiagoSantana@users.noreply.github.com>
2023-03-30 10:45:41 -0300

    exercicio 06

--- client/src/pages/CourseRegisterPage/CourseRegisterPage.jsx
+++ client/src/pages/CourseRegisterPage/CourseRegisterPage.jsx
@@ -1,10 +1,39 @@
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
 import Card from '../../components/Card'
 import InputGroup from '../../components/InputGroup'
 import Button, { BUTTON_VARIANT } from '../../components/Button'
 
 import './CourseRegisterPage.css'
 
+const schema = yup.object().shape({
+  name: yup.string().required('Campo Obrigatorio'),
+  imageUrl: yup.string().required('Campo Obrigatorio'),
+  category: yup.string().required('Campo Obrigatorio'),
+  description: yup.string().required('Campo Obrigatorio'),
+  duration: yup.number('Deve ser um número').required('Campo Obrigatorio'),
+  targetMarket: yup.string().required('Campo Obrigatorio')
+})
+
 function CourseRegisterPage () {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      name: '',
+      imageUrl: '',
+      category: '',
+      description: '',
+      duration: '',
+      targetMarket: ''
+    },
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className ="register-page-container">
       <section className="register-page-section">
@@ -12,18 +41,18 @@ function CourseRegisterPage () {
           <div className="register-page-section-card">
             <h1 className="register-page-section-title">Cadastrar Curso</h1>
 
-            <form className="register-page-section-form">
+            <form className="register-page-section-form" onSubmit={handleSubmit(onSubmit)}>
               <div className="register-page-section-form-row">
                 <div className="register-page-section-form-column">
-                  <InputGroup labelText="Nome" placeholder="Nome do curso"/>
-                  <InputGroup labelText="Duração" placeholder="Duração do curso"/>
-                  <InputGroup labelText="Descrição" placeholder="Descrição do curso"/>
+                  <InputGroup labelText="Nome" placeholder="Nome do curso" helperText={errors?.name?.message} {... register('name')}/>
+                  <InputGroup labelText="Duração" placeholder="Duração do curso" helperText={errors?.name?.message} {... register('duration')}/>
+                  <InputGroup labelText="Descrição" placeholder="Descrição do curso" helperText={errors?.name?.message} {... register('description')}/>
                 </div>
 
                 <div className="register-page-section-form-column">
-                  <InputGroup labelText="Url icone" placeholder="Url contendo icone "/>
-                  <InputGroup labelText="Categoria" placeholder="Categoria do curso"/>
-                  <InputGroup labelText="Público alvo" placeholder="Público alvo do curso"/>
+                  <InputGroup labelText="Url icone" placeholder="Url contendo icone" helperText={errors?.name?.message} {... register('imageUrl')}/>
+                  <InputGroup labelText="Categoria" placeholder="Categoria do curso" helperText={errors?.name?.message} {... register('category')}/>
+                  <InputGroup labelText="Público alvo" placeholder="Público alvo do curso" helperText={errors?.name?.message} {... register('targetMarket')}/>
                 </div>
               </div>
               <div className="register-page-section-form-group">
@@ -34,10 +63,10 @@ function CourseRegisterPage () {
               </div>
               <div className="register-page-section-form-footer">
                 <div>
-                  <Button>Cadastrar</Button>
+                  <Button type="submit">Cadastrar</Button>
                 </div>
                 <div>
-                  <Button variant={BUTTON_VARIANT.PRIMARY_LINK}>Cancelar</Button>
+                  <Button type="submit" variant={BUTTON_VARIANT.PRIMARY_LINK}>Cancelar</Button>
                 </div>
               </div>
             </form>
