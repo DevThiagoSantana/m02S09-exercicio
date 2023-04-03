a996f3de91428d8b6f5ba5c2db9323a0a70c84e0
DevThiagoSantana <113400543+DevThiagoSantana@users.noreply.github.com>
2023-04-03 10:10:08 -0300

    exercicio 05

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { mainTheme } from './styles'
 import Navbar from './components/Navbar'
 import PageWrapper from './components/PageWrapper'
 import Footer from './components/Footer'
@@ -14,19 +16,21 @@ function App() {
   const [globalState, setGlobalState] = useState()
   return (
     <GlobalStateProvider value={[globalState, setGlobalState]}>
-      <BrowserRouter>
-        <Navbar />
-        <PageWrapper>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/course/register" element={<CourseRegisterPage />} />
-            <Route path="/course/:id" element={<CourseDetailsPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </PageWrapper>
-        <Footer />
-      </BrowserRouter>
+      <ThemeProvider theme={mainTheme}>
+        <BrowserRouter>
+          <Navbar />
+          <PageWrapper>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/course/register" element={<CourseRegisterPage />} />
+              <Route path="/course/:id" element={<CourseDetailsPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </PageWrapper>
+          <Footer />
+        </BrowserRouter>
+      </ThemeProvider>
     </GlobalStateProvider>
   )
 }
